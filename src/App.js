66205910d647f83d8f014a63fe1a604335acdbce
Ebot-Jenna <168964308+Ebot-Jenna@users.noreply.github.com>
2024-05-08 10:17:54 +0100

    styled the logo for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import "./App.css";
+import Nav from "./components/nav/Nav"
 
-const App = () => {
-  return (
-    <div>
+function App() {
+  return(
+    <div className="App">
+      <Nav/>
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='Navigation'>
+           <div className="nav-container-1">
+            <div className="nav-icons flex">
+
+              <div className="logo-1">
+                <svg aria-hidden="true" class="logo" focusable="false" viewBox="0 0 24 24" role="img" width="74.48px" height="74.48px" fill="none">
+                  <path fill="currentColor" fill-rule="evenodd" d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+
+              <div className="nav-container-1-icons flex">
+                <div className="nav-icons-group-1 flex">
+
+                  <button><a href="">
+                  <svg aria-hidden="true" class="nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"></path></svg>
+                    </a></button>
+
+                  <button><a href="">
+                  <svg aria-hidden="true" class="nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path></svg>
+                    </a></button>
+
+                  <button><a href="">
+                  <svg aria-hidden="true" class="nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none" data-var="glyph" ><path fill="currentColor" d="M12 3a4.5 4.5 0 00-4.5 4.5H9a3 3 0 013-3V3zM7.5 7.5A4.5 4.5 0 0012 12v-1.5a3 3 0 01-3-3H7.5zM12 12a4.5 4.5 0 004.5-4.5H15a3 3 0 01-3 3V12zm4.5-4.5A4.5 4.5 0 0012 3v1.5a3 3 0 013 3h1.5zM4.5 21v-3H3v3h1.5zm0-3a3 3 0 013-3v-1.5A4.5 4.5 0 003 18h1.5zm3-3h9v-1.5h-9V15zm9 0a3 3 0 013 3H21a4.5 4.5 0 00-4.5-4.5V15zm3 3v3H21v-3h-1.5z"></path></svg>
+                    </a></button>
+                </div>
+
+                <div className="menu">
+                  <button><a href="">
+                  <svg aria-hidden="true" class="nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M21 5.25H3M21 12H3m18 6.75H3"></path></svg></a></button>
+                </div>
+              </div>
+
+            </div>
+         </div>
+      
+
+
+
+    </div>
+  )
+}
+
+export default Nav
