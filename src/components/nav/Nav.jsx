4f70d95d4cea7d46110a9ce4a9cfdaa338ef2c95
Ebot-Jenna <168964308+Ebot-Jenna@users.noreply.github.com>
2024-05-17 17:54:38 +0100

    Finished styling the second part of the navigation at 960px

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -88,19 +88,22 @@ const Nav = () => {
                           </a>
                           </div>
 
-                            <div className="nav-container-2-nav-2-text text">
-                              <span>New & Featured</span>
-                              <span>Men</span>
-                              <span id='show1'>Women</span>
-                              <span className='show2'>Kids</span>
-                              <span className='show2'>Sale</span>
-                              <span id='show3'>Jordan</span>
+                            <div className="nav-container-2-nav-2-text text for-list">
+                              <ul>
+                                <li><a href="">New & Featured</a></li>
+                                <li><a href="">Men</a></li>
+                                <li><a href="">Women</a></li>
+                                <li><a href="">Kids</a></li>
+                                <li><a href="">Sale</a></li>
+                                <li><a href="">Jordan</a></li>
+                              </ul>
+                            
                             </div>
 
                             <div className="nav-container-2-nav-2-icons flex">
                                 <div className="nav-search">
                                   <input type="text" placeholder='Search'/>
-                                  <svg aria-hidden="true" class="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+                                  <svg aria-hidden="true" class="search-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
                                   <path stroke="currentColor" stroke-width="1.5" d="M13.962 16.296a6.716 6.716 0 01-3.462.954 6.728 6.728 0 01-4.773-1.977A6.728 6.728 0 013.75 10.5c0-1.864.755-3.551 1.977-4.773A6.728 6.728 0 0110.5 3.75c1.864 0 3.551.755 4.773 1.977A6.728 6.728 0 0117.25 10.5a6.726 6.726 0 01-.921 3.407c-.517.882-.434 1.988.289 2.711l3.853 3.853"></path>
                                   </svg>
                                 </div>
